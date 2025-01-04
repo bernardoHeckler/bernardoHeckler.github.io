2faba0c7cfcb18df1fc375edfdc85c02c5ccafce
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2025-01-04 14:44:54 -0300

    uma aba para enviar mensagens foi criada

--- scripts/index.js
+++ scripts/index.js
@@ -13,4 +13,11 @@
 //         element.classList.remove('animated');      }
 //     });
 //   });
-  
+window.addEventListener("resize", function () {
+    const elemento = document.querySelector(".mudarTexto");
+    if (window.innerWidth <= 767) {
+      elemento.textContent = "abaixo";
+    } else {
+      elemento.textContent = "ao lado";
+    }
+  });
