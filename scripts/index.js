ee4cade9cddcdb3549075c47987d95be73e74559
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2025-01-06 02:21:04 -0300

    farei um commit para a branch main, atualizei a parte de contato onde o usuario pode me enviar emails, onde eu usei o site do statcforms, isso trará melhor contato e tambem feedback com meus visitantes trazendo um ambiente melhor, fiz uma grande atualização em muitos arquivos.

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,8 @@
-// document.addEventListener('scroll', () => {
-//     const elements = document.querySelectorAll('.asideMensagemAnimacao, .asideChamadaAnimacao');
-  
-//     elements.forEach(element => {
-//       const rect = element.getBoundingClientRect();
-      
-//       const isInViewport = rect.top < window.innerHeight && rect.bottom > 0;
-      
-//       if (isInViewport) {
-//         element.classList.add('animated');
-      
-//       } else {
-//         element.classList.remove('animated');      }
-//     });
-//   });
 window.addEventListener("resize", function () {
-    const elemento = document.querySelector(".mudarTexto");
-    if (window.innerWidth <= 767) {
-      elemento.textContent = "abaixo";
-    } else {
-      elemento.textContent = "ao lado";
-    }
-  });
+  const elemento = document.querySelector(".mudarTexto");
+  if (window.innerWidth <= 767) {
+    elemento.textContent = "abaixo";
+  } else {
+    elemento.textContent = "ao lado";
+  }
+});
